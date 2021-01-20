c8921afe3f77f6ec4782299c5f4bb25b55405a92
Murilo Schünke <mschunke@gmail.com>
2021-01-20 09:48:18 -0300

    Added MAX_CPUS and template.env

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const numCPUs = require('os').cpus().length
 const { createServer } = require('./app')
 
 if (cluster.isMaster) {
-  console.log('Master process initiated! %i CPUs availiable', numCPUs);
-  if (process.env.MULTI_CORE === '1') {
-    for (let i = 0; i < numCPUs; i++) {
+  console.log('Master process initiated!', numCPUs, 'CPUs availiable.');
+  const multiCore = process.env.MULTI_CORE === '1' ? true : false
+  const maxCpus = parseInt(process.env.MAX_CPUS || 0)
+  if (multiCore && maxCpus !== 1) {
+    const maxClusters = numCPUs >= maxCpus ? maxCpus : numCPUs
+    for (let i = 0; i < maxClusters; i++) {
       cluster.fork();
     }
   } else {
